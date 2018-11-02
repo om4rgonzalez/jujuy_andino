d6b26ed770af95df8d0ed8f7974612aa05938ef0
Marcelo <om4r.gonzalez@gmail.com>
2018-11-02 19:53:08 -0300

    test

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1533,7 +1533,7 @@ app.get('/agenda/obtener_eventos_de_usuario/', async function(req, res) {
             .where({ usuario: entrada.data.entrada.usuario, entradaConfirmada: false })
             .exec((err, entradas) => {
                 if (err) {
-                    console.log('El servicio "obtener_eventos_de_usurio" produjo el error: ' + err.message);
+                    console.log('El servicio "obtener_eventos_de_usuario" produjo el error: ' + err.message);
                     return res.json({
                         ok: false,
                         message: 'La busqueda produjo un error: ' + err.message,
